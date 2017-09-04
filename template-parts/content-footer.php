adaf0149968e8ab8c21fd3397fabd3f791ed67d1
Benedict Noel <bjnoel@gmail.com>
2017-09-04 14:56:53 +0800

    Add site footer
    HTML ripped from old site. Need to verify styles/ensure contact form works.

---
+++ template-parts/content-footer.php
@@ -0,0 +1,86 @@
+<div class="region region-footer">
+    <section id="block-block-1" class="block block-block clearfix">
+
+
+        <div class="ea-container-full-width">
+            <div class="ea-container-max-width">
+                <div class="row ea-footer-top">
+                    <div class="col-sm-6">
+                        <h4 class="ea-header">Stay in Touch</h4>
+                        <form action="//eaa.us12.list-manage.com/subscribe/post?u=7fe7ddfe242cb1f12dd20fd38&amp;id=591335048e" method="post" id="mc-embedded-subscribe-form"
+                            name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
+                            <p>
+                                <label for="mce-EMAIL">Email Address</label>
+                            </p>
+                            <p>
+                                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" style="width:400px;max-width:100%" /></p>
+                            <div id="mce-responses">
+                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                <div class="response" id="mce-success-response" style="display:none"></div>
+                            </div>
+                            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <p> <input type="text" name="b_7fe7ddfe242cb1f12dd20fd38_591335048e" tabindex="-1" value="" style="position:absolute;left:-6000px"
+                                /><br /><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"
+                                    class="btn btn-subscribe button" /><br /></p>
+                        </form>
+                        <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
+                        <script type="text/javascript">
+                            <!--//--><![CDATA[// ><!--
+
+                                (function ($) {
+                                    window.fnames = new Array();
+                                    window.ftypes = new Array();
+                                    fnames[1] = 'FNAME';
+                                    ftypes[1] = 'text';
+                                    fnames[3] = 'LNAME';
+                                    ftypes[3] = 'text';
+                                    fnames[0] = 'EMAIL';
+                                    ftypes[0] = 'email';
+                                }(jQuery));
+                            var $mcj = jQuery.noConflict(true);
+
+//--><!]]>
+
+                        </script>
+                    </div>
+                    <div class="col-sm-6">
+                        <div>
+                            <h4 class="ea-header">About Us</h4>
+                            <p>Effective Altruism Australia (ABN 87 608 863 467) is endorsed as a Deductible Gift Recipient
+                                by the Australian Taxation Office. Donations of $2 or more are tax deductible in Australia.</p>
+                        </div>
+                        <div class="ea-social">
+                            <ul>
+                                <li>
+                                    <a href="https://www.facebook.com/effectivealtruismaustralia/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/facebook.png" /></a>
+                                </li>
+                                <li>
+                                    <a href="https://twitter.com/EA_Aus"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/twitter.png" /></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.youtube.com/channel/UCB7RsmYLb7QhsxIGISsqnew"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/youtube.png" /></a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <p class="glyphicon-home ea-glyphicon-inline">Level 7, 41 Exhibition St, Melbourne VIC 3000, Australia</p>
+                            <p class="glyphicon-phone ea-glyphicon-inline">+61 3 8582 4849</p>
+                            <p class="glyphicon-envelope ea-glyphicon-inline"><a href="mailto:info@eaa.org.au">info@eaa.org.au</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="ea-container-full-width  ea-footer-bottom">
+            <div class="ea-container-max-width">
+                <div class="row">
+                    <div class="col-sm-6">© Effective Altruism Australia 2017</div>
+                    <div class="col-sm-6 ea-md-align-right">
+                        <a href="/terms">Terms of use </a>|<a href="/privacy"> Privacy Policy</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+</div>
