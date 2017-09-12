82ace69f71b626d25bbc8ff98533eb370691566e
Benedict Noel <bjnoel@gmail.com>
2017-09-12 14:17:30 +0800

    Fix navbar for mobile
    Set fixed width for drop-down menu

--- js/navigation.js
+++ js/navigation.js
@@ -2,8 +2,8 @@
 /* Adjust opacity of navbar */
 jQuery(window).scroll(function() {
 	if(jQuery(this).scrollTop() > 150) {
-		jQuery('.navbar-fixed-top').addClass('opaque');
+		jQuery('.navbar-header').addClass('opaque');
 	} else {
-		jQuery('.navbar-fixed-top').removeClass('opaque');
+		jQuery('.navbar-header').removeClass('opaque');
 	}
 });
